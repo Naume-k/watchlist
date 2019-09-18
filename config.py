3b1b7e61321b7ba4e6260dc6fb2766408a5c1a66
Naume-k <kobnaome@gmail.com>
2019-09-18 18:26:06 +0200

    added a full watchlist app

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SECRET_KEY = 'a random string'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:nana@1@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    print(SECRET_KEY)
+    print(MAIL_PASSWORD)
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:nana@1@localhost/watchlist_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:nana@1@localhost/watchlist'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY='03d8553e2e71e5ee8b7e044520bbf475'
+export SECRET_KEY='a random string'
+export MAIL_USERNAME='kobnaome@gmail.com'
+export MAIL_PASSWORD='naome4christ@1994'
+python3.6 manage.py server
